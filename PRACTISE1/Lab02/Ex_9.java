ee9f82d1dd437e72ffc1cc379b9c744d8c639d2f
Chayathon Aiadrat <118329856+Chayathon001@users.noreply.github.com>
2022-12-14 11:44:04 +0700

    Delete Ex_9.java

--- PRACTISE1/Lab02/Ex_9.java
+++
@@ -1,7 +0,0 @@
-class CI2P {
-    public static void main(String[] args) {
-       String MyStr = "Patthanan Jaichuai";
-System.out.println("length of MyStr: " + MyStr.length());
-System.out.println(MyStr.toUpperCase());
-    }
-}
